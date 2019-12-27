f26f3029c37283fd335af0c0efca59eada4baf26
tfhgzeo <55290097+tfhgzeo@users.noreply.github.com>
2019-12-26 22:22:28 -0300

    Create menu.js

---
+++ js/menu.js
@@ -0,0 +1,39 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunctiond() {
+    document.getElementById("myDropdownd").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+
+}
+
+function myFunctiona() {
+    document.getElementById("myDropdowna").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+
+}
